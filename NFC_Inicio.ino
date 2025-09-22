4a427199f1cb80fd92e14b412d4ed225cc530861
lemisora <leonardo.michel.dom@gmail.com>
2025-09-21 23:35:44 -0600

    Archivos de prueba

---
+++ NFC_Inicio.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN  21
+#define RST_PIN 4
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Lector inicializado...");
+}
+
+void loop() {
+  if (!mfrc522.PICC_IsNewCardPresent()) return;
+  if (!mfrc522.PICC_ReadCardSerial()) return;
+
+  String uid = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    if (mfrc522.uid.uidByte[i] < 0x10) uid += "0";
+    uid += String(mfrc522.uid.uidByte[i], HEX);
+    if (i < mfrc522.uid.size - 1) uid += " ";
+  }
+  uid.toUpperCase();
+
+  Serial.println(uid);  //
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+}
